fd8b1a6a546349f7e09c88d9fc7c07b207a56927
wangqingxuan <1121586359@qq.com>
2024-09-04 13:14:24 +0800

    密聊项目初始化

---
+++ secret-chat-api/secret-chat-auth/src/main/java/com/itchat/ApplicationRunning.java
@@ -0,0 +1,19 @@
+package com.itchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王哲
+ * @Contact 1121586359@qq.com
+ * @ClassName ApplicationRunning.java
+ * @create 2024年09月04日 下午12:55
+ * @Description 认证服务启动类
+ * @Version V1.0
+ */
+@SpringBootApplication
+public class ApplicationRunning {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunning.class);
+    }
+}
---
+++ secret-chat-api/secret-chat-auth/src/main/java/com/itchat/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.itchat.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 王哲
+ * @Contact 1121586359@qq.com
+ * @ClassName TestController.java
+ * @create 2024年09月04日 下午12:57
+ * @Description 测试控制器
+ * @Version V1.0
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/a")
+    public String a() {
+        return "a";
+    }
+
+}
