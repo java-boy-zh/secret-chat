c7915367abd7842127cdec869b8363d20e5aac8a
wangqingxuan <1121586359@qq.com>
2024-09-05 11:03:36 +0800

    IP拦截器完成

--- secret-chat-gateway/src/main/java/com/itchat/filter/IPLimitFilter.java
+++ secret-chat-gateway/src/main/java/com/itchat/filter/IPLimitFilter.java
@@ -1,6 +1,7 @@
 package com.itchat.filter;
 
 import com.alibaba.nacos.shaded.com.google.gson.Gson;
+import com.itchat.common.BaseInfoProperties;
 import com.itchat.result.GraceJSONResult;
 import com.itchat.result.ResponseStatusEnum;
 import com.itchat.utils.IPUtil;
@@ -29,28 +30,69 @@ import java.nio.charset.StandardCharsets;
  */
 @Component
 @Slf4j
-public class IPLimitFilter implements GlobalFilter, Ordered {
+public class IPLimitFilter extends BaseInfoProperties implements GlobalFilter, Ordered {
 
     /**
      * 判断某个请求的ip在20秒内的请求次数是否超过3次
      * 如果超过3次，则限制访问30秒
      * 等待30秒静默后，才能够继续恢复访问
      */
+
+    private static final Integer continueCounts = 3;
+    private static final Integer timeInterval = 20;
+    private static final Integer limintTimes = 30;
+
+    private static final String ipRedisKey = "secret:chat:gateway:ip";
+    private static final String ipRedisLimitKey = "secret:chat:gateway:ip:limit";
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         log.info("网关 IP拦截器 开始进行拦截~~~");
+
+        return doLimit(exchange, chain);
+    }
+
+    /**
+     * 限制ip请求次数的判断
+     *
+     * @param exchange
+     * @param chain
+     * @return
+     */
+    private Mono<Void> doLimit(ServerWebExchange exchange,
+                               GatewayFilterChain chain) {
         // 获取到请求ip地址
         ServerHttpRequest request = exchange.getRequest();
         String ip = IPUtil.getIP(request);
-        if (1 == 1) {
-            return renderErrorMsg(exchange,ResponseStatusEnum.SYSTEM_ERROR_BLACK_IP);
+        // 正常/黑名单 存放至redis中的键
+        String writeIp = ipRedisKey + ip;
+        String blackIp = ipRedisLimitKey + ip;
+        // 获取当前IP，并查询还剩余多少时间 如果时间大于0 则表示还处在黑名单中
+        long limitTimes = redis.ttl(blackIp);
+        if (limitTimes > 0) {
+            // 中止请求
+            return renderErrorMsg(exchange, ResponseStatusEnum.SYSTEM_ERROR_BLACK_IP);
+        }
+        // 在 redis中对该key进行累加1
+        long writeIpConut = redis.increment(writeIp, 1);
+        // 第一次进来为1 ， 每隔20秒 只能请求3次
+        if (writeIpConut == 1) {
+            redis.expire(writeIp, timeInterval);
+        }
+        // 如果大于3 限制IP
+        if (writeIpConut >= continueCounts) {
+            redis.set(blackIp, blackIp, limintTimes);
+            // 中止请求
+            return renderErrorMsg(exchange, ResponseStatusEnum.SYSTEM_ERROR_BLACK_IP);
         }
         // 放行到后续路由(服务)
         return chain.filter(exchange);
     }
 
+
     /**
      * 重新包装并返回错误信息
+     *
      * @param exchange
      * @param responseStatusEnum
      * @return
@@ -62,7 +104,7 @@ public class IPLimitFilter implements GlobalFilter, Ordered {
         // 构建返回对象
         GraceJSONResult jsonResult = GraceJSONResult.exception(responseStatusEnum);
         // 设置返回header类型
-        if (!response.getHeaders().containsKey("Content-Type")){
+        if (!response.getHeaders().containsKey("Content-Type")) {
             response.getHeaders().add("Content-Type",
                     MimeTypeUtils.APPLICATION_JSON_VALUE);
         }
