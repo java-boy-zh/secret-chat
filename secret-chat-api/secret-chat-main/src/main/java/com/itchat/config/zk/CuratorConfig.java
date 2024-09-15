3de3485010e42507e62905cece6de3ee990f2c52
wangqingxuan <1121586359@qq.com>
2024-09-15 19:00:48 +0800

    注册节点的事件监听,当节点被删除时,从Redis中移除掉启动时放入的端口

--- secret-chat-api/secret-chat-main/src/main/java/com/itchat/config/zk/CuratorConfig.java
+++ secret-chat-api/secret-chat-main/src/main/java/com/itchat/config/zk/CuratorConfig.java
@@ -1,11 +1,17 @@
 package com.itchat.config.zk;
 
 import com.itchat.common.BaseInfoProperties;
+import com.itchat.netty.NettyServerNode;
+import com.itchat.utils.JsonUtils;
+import com.itchat.utils.RedisOperator;
+import jakarta.annotation.Resource;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.CuratorCache;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -25,6 +31,9 @@ public class CuratorConfig extends BaseInfoProperties {
     private String namespace;               // 命名空间（root根节点名称）
 
     public static final String path = "/server-list";
+
+    @Resource
+    private RedisOperator redisOperator;
 
     @Bean("curatorClient")
     public CuratorFramework curatorClient() {
@@ -55,7 +64,60 @@ public class CuratorConfig extends BaseInfoProperties {
 //            e.printStackTrace();
 //         }
 
+        addWatch(path, client);
+
         return client;
+    }
+
+    /**
+     * 注册节点的事件监听
+     */
+    public void addWatch(String path, CuratorFramework client) {
+        CuratorCache curatorCache = CuratorCache.build(client, path);
+        curatorCache
+                .listenable()
+                .addListener((type, oldData, data) -> {
+                    /**
+                     * type : 当前监听到的事件类型
+                     * oldData : 节点更新前的数据和状态(不一定存在)
+                     * data : 节点更新后的数据和状态(不一定存在)
+                     */
+                    log.info("当前监听到的事件类型->{}", type);
+//                    log.info("节点更新前的数据和状态(不一定存在)->{}",oldData);
+//                    log.info("节点更新后的数据和状态(不一定存在)->{}",data);
+
+                    //NODE_CREATED : 节点创建事件
+                    //NODE_CHANGED : 节点更新事件
+                    //NODE_DELETED : 节点删除事件
+
+                    switch (type) {
+                        case NODE_CHANGED -> log.info("触发节点更新事件");
+                        case NODE_CREATED -> log.info("触发节点创建事件");
+                        case NODE_DELETED -> {
+                            log.info("触发节点删除事件");
+
+                            // 节点被删除后 移除Redis中存储的端口号
+                            String oldDataJson = new String(oldData.getData());
+                            if (StringUtils.isNotBlank(oldDataJson)) {
+                                NettyServerNode oldServer =
+                                        JsonUtils.jsonToPojo(oldDataJson, NettyServerNode.class);
+                                log.info("节点更新前的数据和状态(不一定存在),路径->{}", oldData.getPath());
+
+                                String oldPort = oldServer.getPort() + "";
+                                String portKey = "netty_port";
+
+                                redisOperator.hdel(portKey, oldPort);
+                            }
+
+                        }
+                        default -> {
+                            log.info("没有触发任何节点事件");
+                            break;
+                        }
+                    }
+                });
+        // 开启监听
+        curatorCache.start();
     }
 
 }
